8f04c9026843ea681576afdb0f0b5b516be32be2
Hossen <mdmitho199911@gmail.com>
2022-06-04 00:07:27 +0600

    header done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Pages/Home/Home';
+import Header from './components/Pages/Home/Header/Header';
 
 function App() {
   return (
     <div className="App">
-     <Home></Home>
+  <Header></Header>
+     
     </div>
   );
 }
