2c801028894bfebc8b181df88a872bef52456c5d
Hossen <mdmitho199911@gmail.com>
2022-06-04 21:48:24 +0600

    footer done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './components/Pages/Home/Home';
 import Header from './components/Pages/Home/Header/Header';
+import Footer from './components/Pages/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
   <Header></Header>
   <Home></Home>
-     
+     <Footer></Footer>
     </div>
   );
 }
--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Baner from '../Baner/Baner';
+import LetestProduct from '../LetestProduct/LetestProduct';
 
 const Home = () => {
     return (
         <div>
             <Baner></Baner>
+            <LetestProduct></LetestProduct>
         </div>
     );
 };
